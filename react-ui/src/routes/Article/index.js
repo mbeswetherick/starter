b79b6395b3afe310eec0370cef330324c95b44f6
Michael Beswetherick <mbeswetherick@gmail.com>
2018-04-24 23:02:57 -0400

    wowowowowow

--- react-ui/src/routes/Article/index.js
+++ react-ui/src/routes/Article/index.js
@@ -1,13 +1,46 @@
 import React, { Component } from 'react';
 import article from './article';
+import GetSheetDone from 'get-sheet-done'
+
+const DOCUMENT_ID = '19uOLKW3q8vlWQdmnEQWafg0UZP3VW67jWLpN0yCrltA';
 
 class Article extends Component {
+  state = {
+    article: null,
+  };
+
+  componentDidMount() {
+    GetSheetDone.labeledCols(DOCUMENT_ID).then(sheet => {
+      console.log(sheet.data[0].article.split('\n'))
+      this.setState({
+        article: sheet.data[0].article.split('\n'),
+        fetching: false
+      });
+    });
+    fetch('/fetchArticle')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`status ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(json => {
+        this.setState({
+          fetching: false
+        });
+      }).catch(e => {
+        this.setState({
+          fetching: false
+        });
+      })
+  }
+
   render() {
     console.log('what is article ', article)
     return (
       <div>
         <h1>A Hedonistic Vegan's search for lunch in Midtown</h1>
-        {article.map(piece =>  <p>{piece}</p>)}
+        {this.state.article && this.state.article.map(piece =>  <p>{piece}</p>)}
       </div>
     );
   }
--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const path = require('path');
 const cluster = require('cluster');
 const numCPUs = require('os').cpus().length;
 
+const DOCUMENT_ID = '19uOLKW3q8vlWQdmnEQWafg0UZP3VW67jWLpN0yCrltA';
+
 const PORT = process.env.PORT || 5000;
 
 // Multi-process to utilize all CPU cores.
@@ -28,6 +30,15 @@ if (cluster.isMaster) {
   app.get('/api', function (req, res) {
     res.set('Content-Type', 'application/json');
     res.send('{"message":"We out here"}');
+  });
+
+   // Answer API requests.
+   app.get('/fetchArticle', function (req, res) {
+    // GetSheetDone.labeledCols(DOCUMENT_ID).then(sheet => console.log(sheet)).catch(e => {
+    //   console.log('whats the error ', e);
+    // })
+    res.set('Content-Type', 'application/json');
+    res.send('{"article":"Well, we have an article today"}');
   });
 
   // All remaining requests return the React app, so it can handle routing.
